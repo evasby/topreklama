9be446b3227dc9848c91ff6b3fa82d0426932fc9
Vladimir Dmitriev <defurator@mail.ru>
2016-10-04 23:39:08 +0200

    2/3 front

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
 	console.log('+');
+	$('.clients_item').each(function(){
+		$(this).hover(
+			function(){
+				$(this).find('.clients_card').addClass('flip');
+			}, function(){
+				$(this).find('.clients_card').removeClass('flip');
+			}
+		);
+	});
 });
